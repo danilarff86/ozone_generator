edf248f58f3873323c7ff0ade4e8f18fbea625d5
Danylo Lubenets <danilarff86@gmail.com>
2020-03-28 22:56:09 +0200

    cosmetic changes

--- ozone_generator.ino
+++ ozone_generator.ino
@@ -17,8 +17,7 @@ int16_t encoder_value = 0;
 int16_t old_encoder_value = 0;
 bool encoder_pos_changed = false;
 
-const char* main_menu_str = "MAIN MENU";
-uint16_t main_menu_str_left;
+void reboot( );
 
 struct MenuItem
 {
@@ -26,20 +25,12 @@ struct MenuItem
     void ( *fptr )( );
 };
 
-void reboot( );
-
-MenuItem menu_items[]
+const MenuItem menu_items[]
     = {{"Temperature"}, {"Humidity"},    {"Both"},  {"Battery"}, {"Backlight"},
        {"Contrast"},    {"EEPROM dump"}, {"Graph"}, {"Help"},    {"Reboot", reboot}};
-const uint8_t menu_items_count = sizeof( menu_items ) / sizeof( menu_items[ 0 ] );
-const uint8_t menu_items_on_screen = 6;
+
 int8_t menu_mode = -1;
-int16_t menu_line = -1;
-uint8_t menu_start = 0;
 bool menu_item_changed = false;
-const int16_t slider_height = 80;
-unsigned long ms_interval = 1000;
-unsigned long previous_ms = 0;
 
 int16_t tmp_int16;
 uint16_t tmp_uint16;
@@ -60,10 +51,6 @@ setup( )
     lcd.fillScreen( BLACK );
     lcd.setTextSize( 3 );
     lcd.setTextColor( WHITE );
-
-    lcd.getTextBounds( main_menu_str, 0, 0, &tmp_int16, &tmp_int16, &main_menu_str_left,
-                       &tmp_uint16 );
-    main_menu_str_left = ( SCR_WD - main_menu_str_left ) / 2;
 
     Timer1.initialize( 1000 );
     Timer1.attachInterrupt( timerIsr );
@@ -117,16 +104,6 @@ to_main_menu( )
     menu_item_changed = true;
 }
 
-void
-draw_slider( )
-{
-    lcd.drawLine( SCR_WD - 10, lcd.getCursorY( ), SCR_WD - 10, SCR_HT - 10, YELLOW );
-    const auto slider_offset_step
-        = ( SCR_HT - 10 - lcd.getCursorY( ) - slider_height ) / ( menu_items_count - 1 ) + 1;
-    const auto slider_offset = slider_offset_step * menu_line;
-    lcd.fillRect( SCR_WD - 13, lcd.getCursorY( ) + slider_offset, 7, slider_height, BLUE );
-}
-
 void ( *doReset )( void ) = 0;
 void
 reboot( )
@@ -165,6 +142,7 @@ reboot( )
     {
         if ( reboot )
         {
+            lcd.clearScreen( );
             doReset( );
         }
         else
@@ -177,6 +155,12 @@ reboot( )
 void
 handle_main_menu( )
 {
+    static const uint8_t menu_items_count = sizeof( menu_items ) / sizeof( menu_items[ 0 ] );
+    static const uint8_t menu_items_on_screen = 6;
+
+    static int16_t menu_line = -1;
+    static uint8_t menu_start = 0;
+
     const auto prev_menu_line = menu_line;
     menu_line = encoder_value / 2;
     if ( menu_line >= menu_items_count )
@@ -196,7 +180,10 @@ handle_main_menu( )
         return;
     }
 
-    if ( menu_line == prev_menu_line && !check_menu_item_changed( ) )
+    if ( check_menu_item_changed( ) )
+    {
+    }
+    else if ( menu_line == prev_menu_line )
     {
         return;
     }
@@ -211,6 +198,12 @@ handle_main_menu( )
     }
 
     lcd.fillScreen( BLACK );
+
+    static const char* main_menu_str = "MAIN MENU";
+    uint16_t main_menu_str_left;
+    lcd.getTextBounds( main_menu_str, 0, 0, &tmp_int16, &tmp_int16, &main_menu_str_left,
+                       &tmp_uint16 );
+    main_menu_str_left = ( SCR_WD - main_menu_str_left ) / 2;
     lcd.setCursor( main_menu_str_left, 0 );
     lcd.setTextSize( 3 );
     lcd.setTextColor( GREEN );
@@ -219,7 +212,13 @@ handle_main_menu( )
     lcd.drawLine( 0, cursor_y + 10, SCR_WD, cursor_y + 10, MAGENTA );
     lcd.setCursor( 0, cursor_y + 20 );
 
-    draw_slider( );
+    // draw slider
+    static const int16_t slider_height = 80;
+    lcd.drawLine( SCR_WD - 10, lcd.getCursorY( ), SCR_WD - 10, SCR_HT - 10, YELLOW );
+    const auto slider_offset_step
+        = ( SCR_HT - 10 - lcd.getCursorY( ) - slider_height ) / ( menu_items_count - 1 ) + 1;
+    const auto slider_offset = slider_offset_step * menu_line;
+    lcd.fillRect( SCR_WD - 13, lcd.getCursorY( ) + slider_offset, 7, slider_height, BLUE );
 
     for ( uint8_t i = 0; i < menu_items_on_screen; ++i )
     {
@@ -261,6 +260,8 @@ update_encoder( )
 void
 loop( )
 {
+    static unsigned long ms_interval = 1000;
+    static unsigned long previous_ms = 0;
     if ( ( millis( ) - previous_ms ) >= ms_interval )
     {
         // TODO: Implement sensor data update
